ba47a96af78b3970448a0bec1e11dfe065ed0141
SolGarlic <cdascgil@gmail.com>
2017-01-04 22:24:08 +0000

    second commit (check if file exists before downloading)

--- auxfun.R
+++ auxfun.R
@@ -1,10 +1,17 @@
 readData <- function() {
-      top <- read.table("household_power_consumption.txt",
+      fileURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+      temp <- 'household_power_consumption.zip'
+      
+      if (!file.exists(temp)) {
+      download.file(fileURL,temp) }
+
+      top <- read.table(unz(temp, "household_power_consumption.txt"),
                         header=TRUE, sep=";", 
                         colClasses=c("character","character",rep("numeric",7)),
                         na.strings = "?",
                         skip=0,
                         nrows=70000)
+
       top <- subset(top, top$Date=="1/2/2007"|top$Date=="2/2/2007")
       
       top$DateTime <-strptime(paste(top$Date,top$Time),"%d/%m/%Y %H:%M:%S")
